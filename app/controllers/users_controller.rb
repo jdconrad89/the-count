96ad2f6c5836919d2f32272c494f2da77a53ec57
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-02-25 12:33:43 -0700

    Adds the users controller and it's functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to profile_path, success: "You have successfully signed up! Welcome to The Count!"
+    else
+      redirect_to new_user_path, danger: "Failed to create an account, please try again."
+    end
+  end
+
+  def show
+    @user = found_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def found_user
+    User.find(session[:user_id])
+  end
+end
