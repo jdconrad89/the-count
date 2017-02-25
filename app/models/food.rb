060155a317322470c5536390189244326d702859
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-02-25 15:44:06 -0700

    add the food model to change the json into a PORO

---
+++ app/models/food.rb
@@ -0,0 +1,22 @@
+class Food
+  attr_reader :name, :brand,
+              :calories, :fat,
+              :serving_size
+
+  def initialize(food)
+    @name = food[:fields][:item_name]
+    @brand = food[:fields][:brand_name]
+    @calories = food[:fields][:nf_calories].round
+    @fat = food[:fields][:nf_total_fat].round
+    @serving_size = food[:fields][:nf_serving_size_qty]
+  end
+
+
+  def self.find_by_title(food)
+    FoodService.get_food(food).map do |food|
+      new(food)
+    end
+  end
+
+
+end
