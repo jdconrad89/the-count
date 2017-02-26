b82d84c7fb764dc121f839187f7b14f62402a2df
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-02-25 19:39:30 -0700

    add relationship to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :name, :email, presence: true
   validates :email, uniqueness: true
   validates :password, confirmation: true
 
+  has_many :recipes
 end
