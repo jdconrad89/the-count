9a53c9cb198f9b94bcd89ec68d8d289ed7e83871
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-02-25 19:30:19 -0700

    create ingredient and recipe table

---
+++ db/migrate/20170226011846_create_recipes.rb
@@ -0,0 +1,8 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string   :name
+      t.string   :directions
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225195008) do
+ActiveRecord::Schema.define(version: 20170226011846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string  "name"
+    t.string  "brand"
+    t.integer "calories"
+    t.integer "fat"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "directions"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string  "name"
