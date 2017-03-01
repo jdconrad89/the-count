d7eed5761267925ee1ff0b847568afd8e1df798c
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-01 14:20:48 -0700

    add associations tests

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Recipe, type: :model do
     it { is_expected.to validate_presence_of :description }
   end
 
+  context 'associations' do
+    it { is_expected.to belong_to :user }
+    it { is_expected.to have_many :ingredients }
+    it { is_expected.to have_many :recipe_ingredients }
+  end
+
 end
