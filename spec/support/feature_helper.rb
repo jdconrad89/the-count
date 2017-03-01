77f99ee51978cf7ef454eec0e4538242ef95f3e2
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-01 11:01:29 -0700

    add methods to feature_helper to help refactor testing

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -1,9 +1,20 @@
 module FeatureHelper
 
   def logged_in_user
-    user = create(:user, name: 'Waylon Jennings', email:'waylon@music.com', password: 'password' )
-    page.set_rack_session(user_id: user.id)
-    user
+    User.create(name: "Waylon Jennings", email: 'waylon@music.com', password: 'password')
+
+    visit root_path
+    click_on 'Log in'
+    fill_in 'user[email]', with: 'waylon@music.com'
+    fill_in 'user[password]', with: 'password'
+    click_on 'Log me in!'
+  end
+
+  def create_a_recipe
+    click_on "Create a Recipe"
+    fill_in 'recipe[name]', with: 'grilled cheese'
+    fill_in 'recipe[description]', with: 'best sandwich ever'
+    click_on 'Create Recipe'
   end
 
 end
