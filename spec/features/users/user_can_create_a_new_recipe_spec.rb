c407944f84588aa95b775394305685bfc4589a88
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-01 14:20:16 -0700

    add sad path tests for trying to create a recipe without information

--- spec/features/users/user_can_create_a_new_recipe_spec.rb
+++ spec/features/users/user_can_create_a_new_recipe_spec.rb
@@ -29,4 +29,39 @@ feature 'User attempts to log in' do
       expect(page).to have_content("best sandwich ever")
     end
   end
+
+  context 'when a visitor visits their profile' do
+    scenario 'the user leaves name out of a new recipe' do
+      click_on "Create a Recipe"
+
+      expect(page).to have_current_path(new_recipe_path)
+
+      fill_in 'recipe[description]', with: 'best sandwich ever'
+      click_on 'Create Recipe'
+
+      expect(page).to have_current_path(new_recipe_path)
+
+      expect(page).to have_content("Missing information for your recipe please try again.")
+      expect(page).not_to have_content("grilled cheese")
+      expect(page).not_to have_content("best sandwich ever")
+    end
+  end
+
+  context 'when a visitor visits their profile' do
+    scenario 'the user leaves description out of a new recipe' do
+      click_on "Create a Recipe"
+
+      expect(page).to have_current_path(new_recipe_path)
+
+      fill_in 'recipe[name]', with: 'grilled cheese'
+      click_on 'Create Recipe'
+
+      expect(page).to have_current_path(new_recipe_path)
+
+
+      expect(page).to have_content("Missing information for your recipe please try again.")
+      expect(page).not_to have_content("grilled cheese")
+      expect(page).not_to have_content("best sandwich ever")
+    end
+  end
 end
