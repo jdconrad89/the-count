cbd46550552beeb04e02ad649d13c11c64016d5e
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-01 14:21:22 -0700

    add associations tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of :email }
   end
 
+  context 'associations' do
+    it { is_expected.to have_many :recipes }
+  end
 end
