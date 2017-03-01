dff130ee158dfa240e6e2c6c6795209cf02f09ee
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-01 10:56:15 -0700

    Add helper methods for the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
   def recipes?
     current_user.recipes.count == 0
   end
+
+  def recipe_directions(recipe)
+    if recipe.directions.nil?
+      "Enter your directions here!"
+    else
+      "#{recipe.directions}"
+    end
+  end
+
+  def recipe_count
+    current_user.recipes.count
+  end
+
+  def has_ingredients?(recipe)
+    recipe.ingredients.nil?
+  end
+
 end
