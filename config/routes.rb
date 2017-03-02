81bd96b39e9f8a10221d010d0b510a661d016581
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-01 19:25:16 -0700

    fixes lining up issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,28 @@ Rails.application.routes.draw do
   root 'home#index'
 
   #### User Resources ####
-  get '/signup',                          to: 'users#new', as: 'new_user'
-  get '/profile',                         to: 'users#show'
-  resources :users,                       only: [:index, :create]
+  get '/signup',                           to: 'users#new', as: 'new_user'
+  get '/profile',                          to: 'users#show'
+  resources :users,                        only: [:index, :create]
 
 
   #### Session Resources ####
-  get '/login',                           to: 'sessions#new', as: :new_session
-  resources :sessions,                    only: [:create, :destroy]
+  get '/login',                            to: 'sessions#new', as: :new_session
+  resources :sessions,                     only: [:create, :destroy]
 
 
   #### Oauth Singn up routes ####
-  get '/auth/facebook',                   as: :facebook_login
+  get '/auth/facebook',                    as: :facebook_login
   get '/auth/facebook/callback',           to: 'sessions#create'
 
 
   #### Search Resources ####
-  get "/search",                          to: 'search#index', as: 'search'
+  get "/search",                           to: 'search#index', as: 'search'
 
 
   #### Recipe Resources ###
-  resources :recipes,                     only: [:new, :create, :show, :update, :index]
-  namespace :recipes                      do
-    get "/:id/search",                  to: 'search#index', as: 'ingredients_search'
+  resources :recipes,                      only: [:new, :create, :show, :update, :index]
+  namespace :recipes                       do
+    get "/:id/search",                     to: 'search#index', as: 'ingredients_search'
   end
 end
