d3876c73bcaee107af06f7444f7b13cfdef684b9
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-01 20:04:43 -0700

    take care of heroku deployment issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,29 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.addEventListener('dragstart', function(e) {
-    e.target.id = 'drag-' + new Date().getTime()
-    e.dataTransfer.setData('source', e.target.id)
-  })
-  document.addEventListener('dragenter', function(e) {
-    if (e.target.className === 'drop_box') {
-      e.target.style.background = '#6CA6CD'
-    }
-  })
-  document.addEventListener('dragover', function(e) {
-    e.preventDefault()
-  })
-  document.addEventListener('dragleave', function(e) {
-    if (e.target.className === 'drop_box') {
-            e.target.style.background = ''
-    }
-  })
-  document.addEventListener('drop', function(e) {
-    if (e.target.className === 'drop_box') {
-      var data = e.dataTransfer.getData('source')
-      var element = document.getElementById(data)
-      e.target.appendChild(element)
-  }
-})
