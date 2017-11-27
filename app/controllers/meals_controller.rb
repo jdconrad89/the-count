a4b6a5ee38f87b946acdee9c054aade5f914a694
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-11-27 16:49:02 -0700

    add basic functionality for creating and editing meals

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,35 @@
+class MealsController < ApplicationController
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.create(meal_params)
+    @meal.user_id = current_user.id
+    if @recipe.save
+      redirect_to meals_path, success: "Looks like someone was hungry, what did you eat?"
+    else
+      redirect_to new_meal_path, danger: "Missing information for this meal please try again."
+    end
+  end
+
+  def show
+    @meal = found_meal
+  end
+
+  def index
+    @meals = current_user.meals
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :date)
+  end
+
+  def found_meal
+    Meal.find(params[:id])
+  end
+
+end
